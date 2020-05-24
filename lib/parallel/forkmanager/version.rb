0c8c72462f52bdfb4ab23ae2e511dec7985e1690
Nathan Patwardhan <noopy.org@gmail.com>
2020-05-24 14:13:51 -0400

    version bump

--- lib/parallel/forkmanager/version.rb
+++ lib/parallel/forkmanager/version.rb
@@ -3,6 +3,6 @@ module Parallel
     ##
     # This is the version of the Parallel::ForkManager, used in the library and
     # in the rake task for packaging.
-    VERSION = "2.0.5"
+    VERSION = "2.0.6"
   end
 end
